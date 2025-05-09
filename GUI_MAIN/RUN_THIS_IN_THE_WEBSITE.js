032d33000d52e170184e34f5b322568b18e37513
EXTREME-youtube <sterlinghodgeshi@gmail.com>
2025-05-09 15:38:14 -0700

    Create RUN_THIS_IN_THE_WEBSITE.js

---
+++ GUI_MAIN/RUN_THIS_IN_THE_WEBSITE.js
@@ -0,0 +1,5 @@
+let iframe = document.createElement("iframe");
+iframe.src = "https://raw.githubusercontent.com/EXTREME-youtube/GUI/main/GUI_MAIN/GUI(do%20not%20run%20in%20a%20website).html"; // Replace with the actual raw URL
+iframe.width = "600";
+iframe.height = "400";
+document.body.appendChild(iframe);
